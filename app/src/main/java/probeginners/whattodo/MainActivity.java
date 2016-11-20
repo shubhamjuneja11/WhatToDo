7ab5f73cba49ae9719675fce3a39c90dc7affc39
shubham <shubhamjuneja11@gmail.com>
2016-11-21 00:36:21 +0530

    Working on Google Signin

---
+++ app/src/main/java/probeginners/whattodo/MainActivity.java
@@ -0,0 +1,82 @@
+package probeginners.whattodo;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends FragmentActivity implements GoogleApiClient.OnConnectionFailedListener{
+
+    private GoogleSignInOptions gso;
+    private GoogleApiClient mGoogleApiClient;
+    SignInButton signInButton;
+    private static final int RC_SIGN_IN=11;
+    private static final String TAG="hello";
+    String email,name;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+// options specified by gso.
+
+        //enableautomanage removes the overhead of  connecting/disconnecting googleapiclient
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+
+
+    }
+    public void signin(){
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+
+    }
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+           // mStatusTextView.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
+            email=acct.getEmail();
+            name=acct.getDisplayName();
+
+        } else {
+            // Signed out, show unauthenticated UI.
+
+        }
+    }
+}
---
+++ app/src/main/java/probeginners/whattodo/TaskList.java
@@ -0,0 +1,13 @@
+package probeginners.whattodo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TaskList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_list);
+    }
+}
