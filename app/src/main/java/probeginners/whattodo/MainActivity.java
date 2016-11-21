f27e84c41b80bfb6ddc6001e79992bea0808218d
shubham <shubhamjuneja11@gmail.com>
2016-11-22 00:28:29 +0530

    Sign In Done

--- app/src/main/java/probeginners/whattodo/MainActivity.java
+++ app/src/main/java/probeginners/whattodo/MainActivity.java
@@ -3,7 +3,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -12,8 +16,15 @@ import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
-public class MainActivity extends FragmentActivity implements GoogleApiClient.OnConnectionFailedListener{
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
 
     private GoogleSignInOptions gso;
     private GoogleApiClient mGoogleApiClient;
@@ -21,13 +32,33 @@ public class MainActivity extends FragmentActivity implements GoogleApiClient.On
     private static final int RC_SIGN_IN=11;
     private static final String TAG="hello";
     String email,name;
+    private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener authlistner;
+    private Toolbar toolbar;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authlistner);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (authlistner != null) {
+            auth.removeAuthStateListener(authlistner);
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_main);
+        toolbar=(Toolbar)findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
         gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
 
@@ -40,7 +71,29 @@ public class MainActivity extends FragmentActivity implements GoogleApiClient.On
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
 
+        auth=FirebaseAuth.getInstance();
+        authlistner=new FirebaseAuth.AuthStateListener(){
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+
+            }
+        };
         signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signin();
+            }
+        });
 
 
     }
@@ -73,10 +126,39 @@ public class MainActivity extends FragmentActivity implements GoogleApiClient.On
            // mStatusTextView.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
             email=acct.getEmail();
             name=acct.getDisplayName();
+            firebaseAuthWithGoogle(acct);
+
 
         } else {
             // Signed out, show unauthenticated UI.
+            Toast.makeText(MainActivity.this, result.getStatus().toString(), Toast.LENGTH_SHORT).show();
 
         }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+        Log.d("ddd",acct.getIdToken());
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+                        Intent intent=new Intent(MainActivity.this,TaskList.class);
+                        startActivity(intent);
+                        finish();
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        // ...
+                    }
+                });
     }
 }
--- app/src/main/java/probeginners/whattodo/TaskList.java
+++ app/src/main/java/probeginners/whattodo/TaskList.java
@@ -2,6 +2,7 @@ package probeginners.whattodo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class TaskList extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class TaskList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_list);
+        Toast.makeText(this,"sdf",Toast.LENGTH_SHORT).show();
     }
 }
