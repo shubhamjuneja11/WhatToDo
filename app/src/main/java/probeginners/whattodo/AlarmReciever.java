a13b4eb3610a29a0573be29faa244b753b52081d
shubham <shubhamjuneja11@gmail.com>
2017-01-09 22:19:09 +0530

    Default sound set for notification

--- app/src/main/java/probeginners/whattodo/AlarmReciever.java
+++ app/src/main/java/probeginners/whattodo/AlarmReciever.java
@@ -7,7 +7,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.media.RingtoneManager;
 import android.net.Uri;
+import android.provider.Settings;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -22,7 +24,7 @@ public class AlarmReciever extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
        //PendingIntent pendingIntent=PendingIntent.getBroadcast(context,0,intent,0);
         String listname,taskname;
-
+        Uri uri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         listname=intent.getStringExtra("listname");
         taskname=intent.getStringExtra("taskname");
         Log.d("listname",listname);
@@ -32,8 +34,11 @@ public class AlarmReciever extends BroadcastReceiver {
         Notification.Builder builder=new Notification.Builder(context)
                 .setContentTitle("alarm")
                 .setSmallIcon(R.drawable.done)
+
                 //.setSound(Uri.parse("android.resource://" + context.getPackageName() + "/" + R.raw.sanam))
                 .setAutoCancel(true);
+        builder.setSound(uri);
+        //builder.setSound(Settings.System.DEFAULT_NOTIFICATION_URI);
         NotificationManager notificationmanager = (NotificationManager) context
                 .getSystemService(Context.NOTIFICATION_SERVICE);
         // Build Notification with Notification Manager
