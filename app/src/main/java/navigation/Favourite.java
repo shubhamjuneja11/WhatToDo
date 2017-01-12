76bbdb53e0b6ed20a06e2cfb2fa4d7b5732d461d
shubham <shubhamjuneja11@gmail.com>
2017-01-12 19:55:58 +0530

    Navigation drawer layout done

---
+++ app/src/main/java/navigation/Favourite.java
@@ -0,0 +1,16 @@
+package navigation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import probeginners.whattodo.R;
+
+public class Favourite extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favourite);
+
+    }
+}
