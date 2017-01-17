385b86c6249efcecc9317629adac25de1f6f764e
shubham <shubhamjuneja11@gmail.com>
2017-01-18 03:06:04 +0530

    removed email from feedback

--- app/src/main/java/navigation/FeedbackActivity.java
+++ app/src/main/java/navigation/FeedbackActivity.java
@@ -17,12 +17,12 @@ import java.util.regex.Pattern;
 import probeginners.whattodo.R;
 
 public class FeedbackActivity extends AppCompatActivity {
-    EditText name, email, feedback;
+    EditText name,feedback;
     CheckBox checkBox;
     Spinner spinner;
-    String a, b, c, d;
+    String a, c, d;
     boolean f;
-    String developeremailid = "shubhamjuneja11@gmail.com";
+    String developeremailid = "supergeekdeveloper@gmail.com";
     Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +32,7 @@ public class FeedbackActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Feedback Form");
         name = (EditText) findViewById(R.id.name);
-        email = (EditText) findViewById(R.id.email);
+        //email = (EditText) findViewById(R.id.email);
         feedback = (EditText) findViewById(R.id.feedbacktext);
         checkBox = (CheckBox) findViewById(R.id.feedbackcheckbox);
         spinner = (Spinner) findViewById(R.id.spinner);
@@ -41,14 +41,15 @@ public class FeedbackActivity extends AppCompatActivity {
 
     public void submit(View view) {
         a = name.getText().toString().trim();
-        b = email.getText().toString().trim();
+       // b = email.getText().toString().trim();
         c = feedback.getText().toString().trim();
         d = spinner.getSelectedItem().toString().trim();
         f = checkBox.isChecked();
+
         if (a.isEmpty())
             Toast.makeText(this, "Enter a valid name", Toast.LENGTH_SHORT).show();
-        else if (!isValidEmail(b))
-            Toast.makeText(this, "Enter valid email", Toast.LENGTH_SHORT).show();
+        /*else if (!isValidEmail(b))
+            Toast.makeText(this, "Enter valid email", Toast.LENGTH_SHORT).show();*/
         else if (c.isEmpty())
             Toast.makeText(this, "Enter valid Feedback", Toast.LENGTH_SHORT).show();
         else {
@@ -67,16 +68,17 @@ public class FeedbackActivity extends AppCompatActivity {
             }
         }
     }
+    public void open(View view){spinner.performClick();}
 
 
         // validating email id
 
-    private boolean isValidEmail(String email) {
+    /*private boolean isValidEmail(String email) {
         String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
                 + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 
         Pattern pattern = Pattern.compile(EMAIL_PATTERN);
         Matcher matcher = pattern.matcher(email);
         return matcher.matches();
-    }
+    }*/
 }
