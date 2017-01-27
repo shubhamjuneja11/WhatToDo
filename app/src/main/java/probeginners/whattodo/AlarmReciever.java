b72f7822a2206de6f6d7c10f2e80e8d3e5e10e2e
shubham <shubhamjuneja11@gmail.com>
2017-01-28 03:29:31 +0530

    alarm decorated

--- app/src/main/java/probeginners/whattodo/AlarmReciever.java
+++ app/src/main/java/probeginners/whattodo/AlarmReciever.java
@@ -8,15 +8,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.media.MediaPlayer;
-import android.media.RingtoneManager;
 import android.net.Uri;
-import android.provider.Settings;
-import android.util.Log;
-import android.widget.Toast;
 
 import db.DatabaseHandler;
-import navigation.SettingsActivity;
 
 /**
  * Created by junejaspc on 30/11/16.
@@ -25,11 +19,8 @@ import navigation.SettingsActivity;
 public class AlarmReciever extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-       //PendingIntent pendingIntent=PendingIntent.getBroadcast(context,0,intent,0);
         String listname,taskname;
         int taskkey,listkey;
-
-        /*listname=intent.getStringExtra("listname");*/
         taskname=intent.getStringExtra("taskname");
         listname=intent.getStringExtra("listname");
         taskkey=intent.getIntExtra("taskkey",0);
@@ -41,36 +32,20 @@ public class AlarmReciever extends BroadcastReceiver {
                 .setContentText(taskname)
                 .setSmallIcon(R.drawable.logo)
                 .setAutoCancel(true);
-
-
         SharedPreferences sharedPreferences=context.getSharedPreferences("settings", Context.MODE_PRIVATE);
         String chosenRingtone=sharedPreferences.getString("ringtone","");
         boolean vibrate=sharedPreferences.getBoolean("vibrate",false);
-
-
         if(!chosenRingtone.equals("None"))
         builder.setSound(Uri.parse(chosenRingtone));
         if(vibrate)
             builder.setVibrate(new long[] { 1000, 1000, 1000, 1000, 1000 });
-
-
-        // Creates an explicit intent for an Activity in your app
         Intent resultIntent = new Intent(context, TaskDetailsActivity.class);
-        //resultIntent.putExtra("listname",listname);
         resultIntent.putExtra("taskname",taskname);
         resultIntent.putExtra("listkey",listkey);
         resultIntent.putExtra("taskkey",taskkey);
         resultIntent.putExtra("listname",listname);
-
-// The stack builder object will contain an artificial back stack for the
-// started Activity.
-// This ensures that navigating backward from the Activity leads out of
-// your application to the Home screen.
         TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
-// Adds the back stack for the Intent (but not the Intent itself)
         stackBuilder.addParentStack(TaskDetailsActivity.class);
-
-// Adds the Intent that starts the Activity to the top of the stack
         stackBuilder.addNextIntent(resultIntent);
         PendingIntent resultPendingIntent =
                 stackBuilder.getPendingIntent(
@@ -78,12 +53,8 @@ public class AlarmReciever extends BroadcastReceiver {
                         PendingIntent.FLAG_UPDATE_CURRENT
                 );
         builder.setContentIntent(resultPendingIntent);
-
-
-        //builder.setSound(Settings.System.DEFAULT_NOTIFICATION_URI);
         NotificationManager notificationmanager = (NotificationManager) context
                 .getSystemService(Context.NOTIFICATION_SERVICE);
-        // Build Notification with Notification Manager
         notificationmanager.notify(0, builder.build());
 
     }
