6c2f270bdb827716682c904b273f8d712839638c
shubham <shubhamjuneja11@gmail.com>
2017-01-28 03:57:06 +0530

    scheduledtask

--- app/src/main/java/probeginners/whattodo/ScheduledTask.java
+++ app/src/main/java/probeginners/whattodo/ScheduledTask.java
@@ -8,14 +8,13 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -41,108 +40,112 @@ public class ScheduledTask extends AppCompatActivity {
     RecyclerView recyclerView;
     DatabaseHandler handler;
     ScheduledTask.MyAdapter adapter;
-    int positiontoopen,listkey;
+    int positiontoopen, listkey;
 
     ArrayList<Task> list = new ArrayList<>();
 
 
     Cursor cursor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scheduled_task);
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        query = "SELECT * FROM TaskTable a INNER JOIN TaskDetails b ON a.id=b.taskkey WHERE b.alarmstatus= ?";
+        try {
+            setContentView(R.layout.activity_scheduled_task);
+            toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+            recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+            query = "SELECT * FROM TaskTable a INNER JOIN TaskDetails b ON a.id=b.taskkey WHERE b.alarmstatus= ?";
             getSupportActionBar().setTitle("Scheduled Tasks");
 
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onBackPressed();
-                overridePendingTransition(0, R.anim.slide_out_left);
+            toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    onBackPressed();
+                    overridePendingTransition(0, R.anim.slide_out_left);
 
+                }
+            });
+
+            handler = new DatabaseHandler(this);
+            readdatabase = handler.getReadableDatabase();
+            try {
+                cursor = readdatabase.rawQuery(query, new String[]{String.valueOf(1)});
+                if (cursor != null)
+                    preparedata();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        });
+            readdatabase.close();
+            adapter = new ScheduledTask.MyAdapter(list);
 
-        handler = new DatabaseHandler(this);
-        readdatabase = handler.getReadableDatabase();
-        try {
-            cursor = readdatabase.rawQuery(query, new String[]{String.valueOf(1)});
-            if (cursor != null)
-                preparedata();
-            else Log.e("null", "babu");
+            RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this, 1);
+            recyclerView.setLayoutManager(mLayoutManager);
+            recyclerView.addItemDecoration(new GridSpacingItemDecoration(1, dpToPx(0), true));
+            recyclerView.setItemAnimator(new DefaultItemAnimator());
+            recyclerView.setAdapter(adapter);
+
+            recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), recyclerView, new ClickListener() {
+                @Override
+                public void onClick(View view, int position) {
+                    positiontoopen = position;
+
+                }
+
+                @Override
+                public void onLongClick(View view, final int position) {
+
+                    AlertDialog dialog = new AlertDialog.Builder(ScheduledTask.this)
+                            .setTitle("Delete")
+                            .setMessage("Delete Task?")
+                            .setIcon(R.drawable.delete)
+
+                            .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+
+                                public void onClick(DialogInterface dialog, int whichButton) {
+                                    handler.deleteTask(ScheduledTask.this, list.get(position).primary);
+                                    list.remove(position);
+                                    adapter.notifyDataSetChanged();
+                                    dialog.dismiss();
+                                }
+
+                            })
+
+
+                            .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                    dialog.dismiss();
+
+                                }
+                            })
+                            .create();
+                    dialog.show();
+
+                }
+            }));
         } catch (Exception e) {
-            e.printStackTrace();
         }
-        readdatabase.close();
-        adapter = new ScheduledTask.MyAdapter(list);
-
-        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this, 1);
-        recyclerView.setLayoutManager(mLayoutManager);
-        recyclerView.addItemDecoration(new GridSpacingItemDecoration(1, dpToPx(0), true));
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.setAdapter(adapter);
-
-        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), recyclerView, new ClickListener() {
-            @Override
-            public void onClick(View view, int position) {
-                positiontoopen = position;
-                // Log.e("position",position+"");
-                //cardView = (CardView) view;
-            }
-
-            @Override
-            public void onLongClick(View view, final int position) {
-
-                AlertDialog dialog=new AlertDialog.Builder(ScheduledTask.this)
-                        //set message, title, and icon
-                        .setTitle("Delete")
-                        .setMessage("Delete Task?")
-                        .setIcon(R.drawable.delete)
-
-                        .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
-
-                            public void onClick(DialogInterface dialog, int whichButton) {
-                                handler.deleteTask(ScheduledTask.this,list.get(position).primary);
-                                list.remove(position);
-                                adapter.notifyDataSetChanged();
-                                dialog.dismiss();
-                            }
-
-                        })
-
-
-
-                        .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-
-                                dialog.dismiss();
-
-                            }
-                        })
-                        .create();
-                dialog.show();
-
-            }
-        }));
     }
+
     private void preparedata() {
         Task task;
-        if (cursor.moveToFirst()) {
-            do {task=new Task(cursor.getInt(0),cursor.getInt(1),cursor.getString(2), cursor.getString(3), cursor.getInt(4) == 1, cursor.getInt(5) == 1);
-                if(!task.completed)
-                    list.add(0,task);
-                else list.add(list.size(),task);
-            } while (cursor.moveToNext());
+        try {
+            if (cursor.moveToFirst()) {
+                do {
+                    task = new Task(cursor.getInt(0), cursor.getInt(1), cursor.getString(2), cursor.getString(3), cursor.getInt(4) == 1, cursor.getInt(5) == 1);
+                    if (!task.completed)
+                        list.add(0, task);
+                    else list.add(list.size(), task);
+                } while (cursor.moveToNext());
+            }
+
+        } catch (Exception e) {
         }
-
-
     }
 
     private int dpToPx(int dp) {
@@ -150,7 +153,12 @@ public class ScheduledTask extends AppCompatActivity {
         return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
     }
 
-    public  class MyAdapter extends RecyclerView.Adapter<ScheduledTask.MyAdapter.ViewHolder> {
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    public class MyAdapter extends RecyclerView.Adapter<ScheduledTask.MyAdapter.ViewHolder> {
         List<Task> list;
 
 
@@ -168,30 +176,32 @@ public class ScheduledTask extends AppCompatActivity {
 
         @Override
         public void onBindViewHolder(ScheduledTask.MyAdapter.ViewHolder holder, int position) {
-            Task data = list.get(position);
+            try {
+                Task data = list.get(position);
 
-            holder.name.setText(data.getTaskname());
-            holder.listname.setText(data.getlistname());
-            //holder.check.setChecked(data.getflag());
-            if (data.getfavourite()) {
-                holder.favourite.setImageResource(R.drawable.heart);
+                holder.name.setText(data.getTaskname());
+                holder.listname.setText(data.getlistname());
+                if (data.getfavourite()) {
+                    holder.favourite.setImageResource(R.drawable.heart);
 
-            } else {
-                holder.favourite.setImageResource(R.drawable.favourite);
+                } else {
+                    holder.favourite.setImageResource(R.drawable.favourite);
 
-            }
-            if (data.getcompleted()) {
-                holder.check.setChecked(true);
-                holder.name.setPaintFlags(holder.name.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
-                holder.cardView.setCardBackgroundColor(Color.parseColor("#cccccc"));
-                holder.favourite.setBackgroundColor(Color.parseColor("#cccccc"));
-                holder.view.setAlpha(0.6f);
-            } else {
-                holder.check.setChecked(false);
-                holder.name.setPaintFlags(0);
-                holder.cardView.setCardBackgroundColor(getResources().getColor(R.color.white));
-                holder.favourite.setBackgroundColor(getResources().getColor(R.color.white));
-                holder.view.setAlpha(1);
+                }
+                if (data.getcompleted()) {
+                    holder.check.setChecked(true);
+                    holder.name.setPaintFlags(holder.name.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+                    holder.cardView.setCardBackgroundColor(Color.parseColor("#cccccc"));
+                    holder.favourite.setBackgroundColor(Color.parseColor("#cccccc"));
+                    holder.view.setAlpha(0.6f);
+                } else {
+                    holder.check.setChecked(false);
+                    holder.name.setPaintFlags(0);
+                    holder.cardView.setCardBackgroundColor(getResources().getColor(R.color.white));
+                    holder.favourite.setBackgroundColor(getResources().getColor(R.color.white));
+                    holder.view.setAlpha(1);
+                }
+            } catch (Exception e) {
             }
         }
 
@@ -201,90 +211,78 @@ public class ScheduledTask extends AppCompatActivity {
         }
 
         public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-            public TextView name,listname;
+            public TextView name, listname;
             public CheckBox check;
             public ImageButton favourite;
             public CardView cardView;
             public View view;
             public RelativeLayout open;
+
             public ViewHolder(View itemView) {
                 super(itemView);
-                view=itemView;
-                name = (TextView) itemView.findViewById(R.id.name);
-                listname=(TextView)itemView.findViewById(R.id.listname);
-                check = (CheckBox) itemView.findViewById(R.id.check);
-                favourite = (ImageButton) itemView.findViewById(R.id.favourite2);
-                open=(RelativeLayout)itemView.findViewById(R.id.open);
-                cardView = (CardView) itemView;
-                check.setOnClickListener(this);
-                favourite.setOnClickListener(this);
-                open.setOnClickListener(this);
-                //name.setOnClickListener(this);
-
-
+                try {
+                    view = itemView;
+                    name = (TextView) itemView.findViewById(R.id.name);
+                    listname = (TextView) itemView.findViewById(R.id.listname);
+                    check = (CheckBox) itemView.findViewById(R.id.check);
+                    favourite = (ImageButton) itemView.findViewById(R.id.favourite2);
+                    open = (RelativeLayout) itemView.findViewById(R.id.open);
+                    cardView = (CardView) itemView;
+                    check.setOnClickListener(this);
+                    favourite.setOnClickListener(this);
+                    open.setOnClickListener(this);
+                } catch (Exception e) {
+                }
             }
 
             @Override
             public void onClick(View v) {
+                try {
+                    switch (v.getId()) {
+                        case R.id.open:
+                            Intent intent = new Intent(ScheduledTask.this, TaskDetailsActivity.class);
+                            intent.putExtra("listname", list.get(positiontoopen).getlistname());
+                            intent.putExtra("taskname", list.get(positiontoopen).getTaskname());
+                            intent.putExtra("listkey", list.get(positiontoopen).listkey);
+                            intent.putExtra("taskkey", list.get(positiontoopen).getPrimary());
+                            intent.putExtra("decide", "sch");
+                            startActivity(intent);
+                            break;
 
-                switch (v.getId()) {
-                    case R.id.open:
-                        Intent intent = new Intent(ScheduledTask.this, TaskDetailsActivity.class);
-                        intent.putExtra("listname", list.get(positiontoopen).getlistname());
-                        intent.putExtra("taskname", list.get(positiontoopen).getTaskname());
-                        intent.putExtra("listkey", list.get(positiontoopen).listkey);
-                        intent.putExtra("taskkey", list.get(positiontoopen).getPrimary());
-                        intent.putExtra("decide","sch");
-                        startActivity(intent);
-                        break;
+                        case R.id.check:
+                            boolean f = list.get(positiontoopen).getcompleted();
+                            list.get(positiontoopen).putcompleted(!f);
+                            Task task = list.get(positiontoopen);
+                            listkey = task.listkey;
+                            list.remove(positiontoopen);
+                            if (f) {
 
-                    case R.id.check:
-                        Log.e("name", positiontoopen + "");
-                        boolean f = list.get(positiontoopen).getcompleted();
-                        list.get(positiontoopen).putcompleted(!f);
-                        Task task = list.get(positiontoopen);
-                        listkey=task.listkey;
-                        //handler.deleteTask(task);
-                        list.remove(positiontoopen);
-                        if (f) {
+                                list.add(0, task);
+                                adapter.notifyDataSetChanged();
+                            } else {
 
-                            list.add(0, task);
-                            adapter.notifyDataSetChanged();
-                        } else {
-
-                            list.add(list.size(), task);
-                            adapter.notifyDataSetChanged();
-                        }
-                        //handler.addTask(task);
-                        handler.updateTask(task);
+                                list.add(list.size(), task);
+                                adapter.notifyDataSetChanged();
+                            }
+                            handler.updateTask(task);
 
 
-                        if (!f)
-                            // taskdone++;
+                            if (!f)
+                                handler.ChangeTaskCount(listkey, true);
 
-                            handler.ChangeTaskCount(listkey,true);
+                            else
+                                handler.ChangeTaskCount(listkey, false);
+                            break;
 
-                        else //taskdone--;
-                            handler.ChangeTaskCount(listkey,false);
-                        //if(check.isChecked())
-                        //handler.changeListTaskDone(listname,list.get(positiontoopen).getTaskname(),true);
-                        // handler.changeListTaskDone(listkey, taskdone);
-                        break;
-
-                    case R.id.favourite2:
-                        list.get(positiontoopen).putfavourite(!list.get(positiontoopen).getfavourite());
-                        handler.updateTask(list.get(positiontoopen));
-                        //list.remove(positiontoopen);
-
-                        break;
+                        case R.id.favourite2:
+                            list.get(positiontoopen).putfavourite(!list.get(positiontoopen).getfavourite());
+                            handler.updateTask(list.get(positiontoopen));
+                            break;
+                    }
+                    adapter.notifyDataSetChanged();
+                } catch (Exception e) {
                 }
-                adapter.notifyDataSetChanged();
-
             }
         }
-    }
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
     }
 }
